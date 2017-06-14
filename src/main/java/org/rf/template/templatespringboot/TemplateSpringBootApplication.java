c8b18b43364c9b9540e9368d9ac3890ffd4ac5e2
ctwoprod <ctwoprod@gmail.com>
2017-06-14 22:43:17 +0700

    initial commit code

---
+++ src/main/java/org/rf/template/templatespringboot/TemplateSpringBootApplication.java
@@ -0,0 +1,12 @@
+package org.rf.template.templatespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/org/rf/template/templatespringboot/TemplateSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package org.rf.template.templatespringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TemplateSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
